98e98ca6c636283bff920fe5639fcd5bb88ffbc5
Benlove Anelus <anelusbenlove@gmail.com>
2021-09-06 23:39:42 -0300

    Classe-corr

--- Back-end/modelo_cadastro.py
+++ Back-end/modelo_cadastro.py
@@ -1,4 +1,5 @@
 from config import * 
+# modelos
 
 class Paciente(db.Model):
     #Atributos do paciente
@@ -34,8 +35,8 @@ class Paciente(db.Model):
             "sobrenome": self.sobrenome
         }
 
-
 class Medico(db.Model):
+
     #Atributos do médico
     id_medico = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(255))
@@ -67,11 +68,35 @@ class Medico(db.Model):
             "paciente": self.paciente.json()
         }
         
+class ConsultaRealizado(db.Model):
+    id_consulta = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(255)) # data da consulta
+    motivo = db.Column(db.String(255)) # motivo: consulta por dor de cabeça, de barriga
+
+    # atributo de chave estrangeira
+    paciente_id = db.Column(db.Integer, db.ForeignKey(Paciente.id_paciente), nullable=False)
+    # atributo de relacionamento, para acesso aos dados via objeto
+    paciente = db.relationship("Paciente")    
+
+    
+    def __str__(self): 
+        # expressão da classe em forma de texto
+        return f"{self.data}, {self.motivo}, {self.paciente}" 
+
+    def json(self):
+        # expressao da classe no formato json
+        return {
+            "id": self.id_consulta,
+            "data": self.data,
+            "motivo": self.motivo,
+            "paciente_id": self.paciente_id,
+            "paciente": self.paciente.json()
+        }
 
 
-#Teste das classes
+#Testes das classes
 if __name__ == "__main__":
-    #Apaga o BD, se houver
+    #Apaga o arquivo .bd, se houver
     if os.path.exists(arquivobd):
         os.remove(arquivobd)
     
@@ -79,10 +104,10 @@ if __name__ == "__main__":
     db.create_all()
 
     #Teste da classe Paciente
+    print("\nPacientes: ")
     paciente1 = Paciente(nome = "Carlos", sobrenome = "Landeira")
     paciente2 = Paciente(nome = "Gabriel", sobrenome = "Speckart")
     paciente3 = Paciente(nome = "Benlove", sobrenome = "Anelus")
-    
     
     #Persistir
     db.session.add(paciente1)
@@ -101,12 +126,12 @@ if __name__ == "__main__":
 
 
     #Teste da classe Medico
+    print("\nMédicos: ")
     medico1 = Medico(nome = "Paulo", sobrenome = "McCartney", cpf = "012.546.213-10", data_nasc = "12/09/1974", sexo = "Masculino", paciente=paciente1)
     medico2 = Medico(nome = "João", sobrenome = "Lennon", cpf = "845.685.489-95", data_nasc = "26/07/1968", sexo = "Masculino", paciente=paciente2)
     medico3 = Medico(nome = "Jorge", sobrenome = "Harrison", cpf = "365.781.259.58", data_nasc = "08/04/1984", sexo = "Masculino", paciente=paciente3)
     
-    
-
+    #Persistir
     db.session.add(medico1)
     db.session.add(medico2)
     db.session.add(medico3)
@@ -119,3 +144,7 @@ if __name__ == "__main__":
         print(f"\nOlá, {medico.nome}, seu paciente é {medico.paciente.nome}")
         #Exibe a classe em json
         print(medico.json())
+
+    # teste da classe ConsultaRealizado
+
+    print("\nConsultas realizados: ")
