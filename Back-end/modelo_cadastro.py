fea14b8e4c6fa58900998fe6081f48efca6b0c15
Benlove Anelus <anelusbenlove@gmail.com>
2021-10-23 23:13:21 -0300

    exame

--- Back-end/modelo_cadastro.py
+++ Back-end/modelo_cadastro.py
@@ -109,6 +109,49 @@ class MarcarConsulta(db.Model):
             "medico": self.medico.json()
         }
 
+class Exame(db.Model):
+    id_exame = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(254)) # nome do exame
+    unidade = db.Column(db.String(254)) # unidade de medida
+    vr = db.Column(db.String(254)) # valores de referência
+    def __str__(self):
+        return f"{self.nome} [{self.id_exame}], unidade={self.unidade} ({self.vr})"  
+    def json(self):
+        return {
+            "id_exame":self.id_exame,
+            "nome":self.nome,
+            "unidade":self.unidade,
+            "vr":self.vr
+        }
+    
+
+class ExameRealizado(db.Model):
+    id_exame = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(254)) # data do exame
+    resultado = db.Column(db.String(254)) # apenas o valor
+    
+    # pessoa que fez o exame; não pode ser nulo (composição!)
+    paciente_id_exame = db.Column(db.Integer, db.ForeignKey(Paciente.id), nullable=False)
+    paciente = db.relationship("Paciente")
+    # exame que foi realizado; não pode ser nulo (composição!)
+    exame_id =  db.Column(db.Integer, db.ForeignKey(Exame.id), nullable=False)
+    exame = db.relationship("Exame")
+
+    def __str__(self): # expressão da classe em forma de texto
+        return f"{self.data}, {self.resultado}, " + \
+            f"{self.pessoa}, {self.exame}"
+
+    def json(self):
+        return {
+            "id":self.id,
+            "data":self.data,
+            "resultado":self.resultado,
+            "pessoa_id":self.pessoa_id,
+            "pessoa":self.pessoa.json(),
+            "exame_id":self.exame_id,
+            "exame":self.exame.json()
+        }
+
 #Testes das classes
 if __name__ == "__main__":
     #Apaga o arquivo .bd, se houver
